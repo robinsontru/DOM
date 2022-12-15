f506216405c90a2698aa1eac8b5370499d8a190f
robinson truque <robinsontruque04@gmail.com>
2022-12-15 13:09:34 -0500

    fb

---
+++ js/face.js
@@ -0,0 +1,223 @@
+var body = document.querySelector('body')
+console.log(body)
+var main = document.createElement('main');
+body.appendChild(main);
+
+const di = document.querySelectorAll('.demo-class');
+console.log(di);
+var democlass3 = document.createElement('div');
+democlass3.classList.add('.conty')
+console.log(democlass3)
+main.appendChild(democlass3)
+//
+const text = document.querySelectorAll('.demo-class');
+var palabras = document.createElement('p');
+palabras.classList.add('.container')
+main.appendChild(palabras)
+const texto = document.createTextNode('facebook');
+palabras.appendChild(texto)
+//
+const div = document.querySelectorAll('.demo-class');
+console.log(div);
+var democlass = document.createElement('div');
+democlass.classList.add('.cont')
+console.log(democlass)
+main.appendChild(democlass)
+
+//
+const inicio = document.querySelectorAll('demo-class')
+var button = document.createElement('button')
+button.classList.add('inicio')
+democlass.appendChild(button)
+button.addEventListener('click', () => {
+    console.log('hisites click');
+})
+//style
+
+body.style.backgroundColor = '247, 241, 186'
+
+main.style.marginLeft = '10%'
+main.style.marginTop = '20px'
+main.style.marginRight = '10%'
+palabras.style.color = 'dodgerblue'
+palabras.style.fontSize = '30px'
+palabras.style.height = '30%'
+palabras.style.textAlign = 'star'
+button.style.backgroundColor = 'white'
+button.style.borderRadius = '30px'
+button.style.width = '50%'
+button.style.height = '150%'
+button.style.border = 'transparent'
+button.style.fontSize = '5vh'
+democlass.style.textAlign = 'space-between'
+
+
+
+
+//Facebook te ayuda a comunicarte y compartir con las personas que forman parte de tu vida.
+const texto1 = document.createTextNode('Facebook te ayuda a comunicarte y compartir con las personas que forman parte de tu vida.')
+button.appendChild(texto1)
+//
+const div1 = document.querySelectorAll('.demo-class');
+console.log(div);
+var democlass1 = document.createElement('div');
+democlass1.classList.add('.contt')
+main.appendChild(democlass1)
+//button 
+const medio = document.querySelectorAll('demo-class')
+var button = document.createElement('button')
+button.classList.add('inicio')
+democlass1.appendChild(button)
+button.addEventListener('click', () => {
+    console.log('hisites click');
+})
+//correo eletrónico o número de teléfono
+const texto2 = document.createTextNode('correo eletrónico o número de teléfono')
+button.appendChild(texto2)
+button.style.width = '20%'
+button.style.height = '40px'
+button.style.borderRadius = '30px'
+button.style.marginTop = '15px'
+democlass1.style.textAlign = 'end'
+//password
+const div3 = document.querySelectorAll('.demo-class');
+var democlase = document.createElement('div');
+democlase.classList.add('.contenido')
+main.appendChild(democlase)
+var input = document.createElement('input')
+democlase.appendChild(input)
+//
+democlase.style.textAlign = 'end'
+democlase.style.marginTop = '15px'
+input.style.marginTop = '15px'
+input.placeholder = 'contraseña'
+input.style.width = '20%'
+input.style.height = '40px'
+input.style.textAlign = 'center'
+input.style.borderRadius = '30px'
+input.style.color = 'red'
+//siguiente
+var demo = document.createElement('div');
+demo.classList.add('.appe')
+main.appendChild(demo)
+//button 
+const intermedio = document.querySelectorAll('demo-class')
+var button = document.createElement('button')
+button.classList.add('sigiente')
+demo.appendChild(button)
+button.addEventListener('click', () => {
+    console.log('hisites click');
+})
+const wit = document.createTextNode('inicio secion')
+button.appendChild(wit)
+//
+demo.style.textAlign = 'end'
+demo.style.marginTop = '15px'
+button.style.width = '20%'
+button.style.height = '40px'
+button.style.borderRadius = '30px'
+button.style.backgroundColor = 'dodgerblue'
+button.style.color='white'
+
+
+//olvido su contraseña
+var forgot = document.createElement('div');
+forgot.classList.add('.apple')
+main.appendChild(forgot)
+//button 
+const final = document.querySelectorAll('demo-class')
+var button = document.createElement('button')
+button.classList.add('final')
+forgot.appendChild(button)
+button.addEventListener('click', () => {
+    console.log('hisites click');
+})
+const modified = document.createTextNode('¿ Has Olvidado Tu Contraseña?')
+button.appendChild(modified)
+//
+forgot.style.textAlign = 'end'
+forgot.style.marginTop = '15px'
+button.style.width = '20%'
+button.style.height = '40px'
+button.style.borderRadius = '0px'
+button.style.backgroundColor = 'transparent'
+button.style.color = 'dodgerblue'
+button.style.borderColor = 'white'
+
+//new cu
+var forgot = document.createElement('div');
+forgot.classList.add('.apple')
+main.appendChild(forgot)
+//button 
+const casifinal = document.querySelectorAll('demo-class')
+var button = document.createElement('button')
+button.classList.add('casifinal')
+forgot.appendChild(button)
+button.addEventListener('click', () => {
+    console.log('hisites click');
+})
+
+const cuenta = document.createTextNode('¿ Crear una nueva cuenta ?')
+button.appendChild(cuenta)
+//
+forgot.style.textAlign = 'end'
+forgot.style.marginTop = '15px'
+button.style.width = '20%'
+button.style.height = '40px'
+button.style.borderRadius = '30px'
+button.style.backgroundColor = 'green'
+button.style.color = 'white'
+button.style.borderColor = 'white'
+
+
+//no tienes cuenta
+var bill = document.createElement('div');
+bill.classList.add('.bill')
+main.appendChild(bill)
+
+
+//
+const registar = document.querySelectorAll('demo-class')
+var letras = document.createElement('p')
+letras.classList.add('unregistered')
+bill.appendChild(letras)
+const cont = document.createTextNode('¿crear una pagina?')
+letras.appendChild(cont)
+
+//
+const regis = document.querySelectorAll('demo-class')
+var letras1 = document.createElement('p')
+letras1.classList.add('register')
+bill.appendChild(letras1)
+
+const conte = document.createTextNode('para un famoso, marca o una empresas ')
+letras1.appendChild(conte)
+//
+bill.style.display = 'flex'
+bill.style.marginLeft = '40%'
+bill.style.marginTop = '15px'
+letras.style.color = 'blanck'
+letras.style.fontSize = '15px'
+letras1.style.color = 'grey'
+letras1.style.fontSize = '15px'
+bill.style.justifyContent='end'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
